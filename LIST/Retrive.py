3110b1b2b862a279b2af12c14ea60e452d98f99f
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2026-04-21 20:08:57 +0530

    CURD

---
+++ LIST/Retrive.py
@@ -0,0 +1,9 @@
+lis = [1,2,4,5,6,6]
+print(lis)
+print("---------------------")
+print(lis[4])
+print("-------------------")
+print(lis[1:5:1])
+print("---------------------")
+for x in lis :
+	print(x)
---
+++ LIST/Update.py
@@ -0,0 +1,14 @@
+lis = [1,2,4,6,8,4]
+print(lis)
+lis.append({"sai":1,"mpl" : 2})
+print(lis)
+
+print("--------------------------------")
+
+lis.extend({1,2,3,4,5,6})
+print(lis)
+
+print("----------------------------------")
+
+lis.insert(3,[ ])
+print(lis)
