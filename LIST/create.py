8ddc130bacaa296071ba0dc77e0ee84a195ca1bc
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2026-04-21 18:45:12 +0530

    CURD

---
+++ LIST/create.py
@@ -0,0 +1,31 @@
+lis1 = [1,2,3,4,5]
+print(lis1)
+print(type(lis1))
+
+print("---------------------")
+
+lis2=[ ]
+print(lis2)
+print(type(lis2))
+
+print("---------------------")
+
+
+type_cast = list(range(10))
+print(type_cast)
+print(type(type_cast))
+
+print("----------------------")
+
+lis3 = eval(input("Enter elements : "))
+print(lis3)
+print(type(lis3))
+
+print("--------------------")
+
+str= " VJ INNOVATIVE SOFTWARE SOLUTIONS PVT LTD"
+print(str)
+print(type(str))
+lis4= str.split(" ")
+print(lis4)
+print(type(lis4))
